cbdd9f4f63a724f04be338653c8c81d2b45a0dc3
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-17 14:07:50 -0700

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,20 @@
+// 5 variables of different data types
+// string, number, array, boolean &&
+// object(4 key value pairs) string, number, array, boolean
+
+const myString = 'Hello this is my string!';
+const myNum = 25000;
+const myArray = [1, 'b', 9, true, 'false'];
+const myBoolean = false;
+const myObject = {name: 'Barent', age: 30, colors: ['green', 'blue', 'black'], anxious: true};
+
+let myString2 = 'this is my second string';
+let myNum2 = 3930;
+let myArray2 = [1, 5, 'turtles', 3];
+let myBoolean2 = true;
+let myObject2 = {
+    name: 'Barent again',
+    age: 30,
+    colors: ['black', 'white'],
+    anxious: true
+};
